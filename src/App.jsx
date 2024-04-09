b0a074035c00176a09157cab774fe8721dda8e78
Faisal <138551448+FaisalNadaf@users.noreply.github.com>
2024-04-09 21:27:02 +0530

    Hero6 completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero2 from "./components/Hero2";
 import Hero3 from "./components/Hero3";
 import Hero4 from "./components/Hero4";
 import Hero5 from "./components/Hero5";
+import Hero6 from "./components/Hero6";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Hero3 />
       <Hero4 />
       <Hero5 />
+      <Hero6 />
     </div>
   );
 }
---
+++ src/components/Hero6.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+const Hero6 = () => {
+  return (
+    <div className=" h-screen bg-[#509995]">
+      <div className="text-center py-10">
+        <p className="text-5xl font-bold text-white pt-6">Pricing</p>
+        <p className="px-[30%] pt-8 font-semibold text-gray-300">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas iusto
+          itaque assumenda. Ipsum, blanditiunt repellendus tempore alias atque
+          nulla!is sunt repellendus tempore alias atque nulla!
+        </p>
+      </div>
+      <div className="h-1/2 flex items-center justify-evenly">
+        <div className="bg-white text-[#509995] w-1/5 h-full text-center rounded shadow-lg">
+          <div className="py-6">
+            <p className="text-2xl font-bold">NEWBIE CLASS</p>
+            <p className="text-2xl font-bold">$00.00</p>
+          </div>
+          <i class="fa-solid fa-medal text-7xl pb-7"></i>
+          <p className="pb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+          <button className="text-white px-14 rounded py-2 bg-[#509995] font-bold ">
+            REGISTER NOW
+          </button>
+        </div>
+        <div className="bg-white text-[#509995] w-1/5 h-full text-center rounded shadow-lg">
+          <div className="py-6">
+            <p className="text-2xl font-bold">NEWBIE CLASS</p>
+            <p className="text-2xl font-bold">$00.00</p>
+          </div>
+          <i class="fa-solid fa-medal text-7xl pb-7"></i>
+          <p className="pb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+          <button className="text-white px-14 rounded py-2 bg-[#509995] font-bold ">
+            REGISTER NOW
+          </button>
+        </div>
+        <div className="bg-white text-[#509995] w-1/5 h-full text-center rounded shadow-lg">
+          <div className="py-6">
+            <p className="text-2xl font-bold">NEWBIE CLASS</p>
+            <p className="text-2xl font-bold">$00.00</p>
+          </div>
+          <i class="fa-solid fa-medal text-7xl pb-7"></i>
+          <p className="pb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+          <button className="text-white px-14 rounded py-2 bg-[#509995] font-bold ">
+            REGISTER NOW
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero6;
